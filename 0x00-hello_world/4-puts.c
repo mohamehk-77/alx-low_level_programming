61ca8c0be8f35ab7c07a211f5cb76c5d813929d1
mohamed <kmido9729@gmail.com>
2023-08-11 01:56:50 +0200

    ff

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main- printing a specific
+ *
+ * Return: (success) 0
+ */
+int main(void)
+{
+puts("\"pgramming is like building a multilingual puzzle");
+return (0);
+}
