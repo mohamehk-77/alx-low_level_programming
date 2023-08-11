d1608c39f20a4e6d51bc9cbac1e504a87faf44e5
mohamed <kmido9729@gmail.com>
2023-08-11 03:17:36 +0200

    a7777777aa

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,8 @@
  *
  * Return: (success) 0
  */
- int main(void)
- {
-    printf("with proper grammar, but the outcome is a piece of art,");
-    return(0);
- }
- 
- 
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
