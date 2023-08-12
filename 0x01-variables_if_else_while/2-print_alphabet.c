c7dfb15d2a1ae3cdca4bbbc234c89d0b23e76993
mohamed <kmido9729@gmail.com>
2023-08-12 03:25:14 +0200

    moo

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - program start
+ *
+ * while function in charge
+ *
+ * Retrun: (scuccss) 0
+*/
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
