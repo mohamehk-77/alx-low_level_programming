ca9b0d8cf981ed7aca4b403af767370f84c298a3
mohamed <kmido9729@gmail.com>
2023-08-12 17:01:18 +0200

    moo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - start point
+ *
+ * prints 0, 1, - 9with putchar function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+putchar(digit + 48);
+if (digit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+digit++;
+}
+putchar('\n');
+return (0);
+}
