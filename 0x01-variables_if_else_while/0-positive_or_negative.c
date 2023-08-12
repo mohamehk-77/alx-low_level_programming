9cd26ca6bf9db10f3e693dd0a82864d147dfc58a
mohamed <kmido9729@gmail.com>
2023-08-12 03:02:47 +0200

    moo

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdio.h>
 #include <time.h>
 /**
  * main -  random of number generator
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - The random of number Generetor
+ *
+ * int n; random Generate
+ *
+ * Return: (sccuess) 0
+ *
+*/
+int main(void)
+{
+int n, digit;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+digit = n % 10;
+if (digit > 5)
+printf("Last digit of %i is %i and is greater than 5\n", n, digit);
+else if (digit == 0)
+printf("Last digit of %i is %i and is 0\n", n, digit);
+else
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
+return (0);
+}
