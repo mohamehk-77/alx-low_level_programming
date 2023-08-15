df29629f5ad441a2ca4a02b11a775b31b0e61e90
mohamed <kmido9729@gmail.com>
2023-08-15 21:48:15 +0200

    ii

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,8 +3,11 @@
 /**
  * print_alphabet - prints the alphabet, in lowercase ues _putchar
 */
-void print_alphabet(void);
-int ch;
-for (ch = 'a'; ch <= 'z' ch++)
+void print_alphabet(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
 _putchar(ch);
 _putchar('\n');
+}
