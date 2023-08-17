eb5e97aa3edef7783d1df72458a93d7b41737d63
mohamed <kmido9729@gmail.com>
2023-08-18 01:24:45 +0200

    more

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,9 +7,9 @@
 void print_numbers(void)
 {
 	int num;
-		for (num = 0; num >= 9; num++)
+		for (num = 0; num > 10; num++)
 		{
-			_putchar(num + 48);
+			_putchar(num + 0);
 		}
 _putchar('\n');
 }
