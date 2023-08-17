c5af24992afae43dcf82e6ca9f50be3e0e94cf0a
mohamed <kmido9729@gmail.com>
2023-08-17 20:51:17 +0200

    more

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+##0x04-more_functions_nested_loops
+
+10 tasksc
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <ctype.h>
+int _putchar(char c);
+int _isupper(int c);
+
+
+
+
+
+#endif
