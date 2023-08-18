b41e512bf7dc1a86cb5170e995e067fe963e910d
mohamed <kmido9729@gmail.com>
2023-08-18 02:19:20 +0200

    more

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - print 0 -9 excpt 2 , 4
+ * use _putchar fanctions
+ * Return: Always 0 (success)
+*/
+
+void print_most_numbers(void)
+{
+	int num = 0;
+
+		while (num <= 9)
+		{
+			if (num != 2 && num != 4)
+			{
+				_putchar(num + '0');
+			}
+				num++;
+		}
+				_putchar('\n');
+}
