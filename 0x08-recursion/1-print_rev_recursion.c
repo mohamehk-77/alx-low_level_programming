7bea007fea956f8287b6443cba8b766c5c0bef21
mohamed <kmido9729@gmail.com>
2023-08-30 21:36:50 +0200

    pointeer

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - that print string in reverse
+ * @s: its pointer
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
