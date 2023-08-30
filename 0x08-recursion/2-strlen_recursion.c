5e1533ad39f8853bb6a9809d2ce94c46d6e03433
mohamed <kmido9729@gmail.com>
2023-08-30 23:25:27 +0200

    pointeer

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,11 +2,12 @@
 /**
  * _strlen_recursion - that returns the length of a string
  * @s: its pointer
+ * Return: The length of the string.
 */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-
+	else
 		return (1 + _strlen_recursion(s + 1));
 }
