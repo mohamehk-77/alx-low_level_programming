31170b7b7ebbefd73aa9975e4fa32783abb85c3f
mohamed <kmido9729@gmail.com>
2023-09-02 01:43:44 +0200

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ * @argc: argyment 1
+ * @argv: argyment 2
+ * Return: 0
+*/
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
