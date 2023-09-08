11f5683015c692d3009667da094eed874ed571a8
mohamedk-77 <kmido9729@gmail.com>
2023-09-08 21:36:48 +0200

    more malloc

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -18,7 +18,7 @@ char *argstostr(int ac, char **av)
 
 	for (i = 0, cont = 0; i < ac; i++)
 	{
-		for (j = 0; *(*(av + i) + j) != '\0'; j++, total++)
+		for (j = 0; *(*(av + i) + j) != '\0'; j++, cont++)
 			;
 		cont++;
 	}
