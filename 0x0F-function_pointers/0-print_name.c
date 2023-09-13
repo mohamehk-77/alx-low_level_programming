5c3707931b3deb47c8be04d6c14a97b82763ddab
mohamedk-77 <kmido9729@gmail.com>
2023-09-13 12:20:22 +0200

    function--pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - starting point
+ * @f: 1
+ * @name: 2
+ * Return:
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+	{
+		return;
+		f(name);
+	}
+}
