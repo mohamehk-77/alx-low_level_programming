613ec2ca14b5ec3483b60583a29b757341420bad
mohamedk-77 <kmido9729@gmail.com>
2023-09-13 20:21:54 +0200

    function--pointer

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - check point
+ * @a: first intger
+ * @b: second
+ * Return: 0
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - check point
+ * @a: first intger
+ * @b: second
+ * Return: 0
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - check point
+ * @a: first intger
+ * @b: second
+ * Return: 0
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - check point
+ * @a: first intger
+ * @b: second
+ * Return: 0
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - check point
+ * @a: first intger
+ * @b: second
+ * Return: 0
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
