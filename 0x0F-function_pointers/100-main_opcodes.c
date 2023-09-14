512c44a19b623931f0449d0dbfd6b891edb7e1d0
mohamedk-77 <kmido9729@gmail.com>
2023-09-14 21:15:48 +0200

    function--pointer

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - operates on 2 positive numbers and print the result
+* @argc: argument 1
+* @argv: argument 2
+* Return: 0 if function runs properly.
+*/
+int main(int argc, char *argv[])
+{
+char *add;
+int i, bytes;
+
+if (argc != 2)
+{
+	printf("Error\n");
+	exit(1);
+}
+	if (atoi(argv[1]) < 0)
+{
+	printf("Error\n");
+	exit(2);
+}
+
+	add = (char *)&main;
+
+	bytes = atoi(argv[1]);
+
+i = 0;
+
+	while (i < bytes - 1)
+{
+	printf("%02hhx ", add[i]);
+	i++;
+}
+
+	printf("%02hhx\n", add[i]);
+	return (0);
+}
