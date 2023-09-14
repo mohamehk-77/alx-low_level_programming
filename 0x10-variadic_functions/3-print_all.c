c50241990a1fb876e6604e773605217c93cab310
mohamedk-77 <kmido9729@gmail.com>
2023-09-15 01:40:00 +0200

    va_list

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,42 @@
+#include "variadic_functions.h"
+/**
+ * print_all - Prints arguments based on a format string.
+ * @format: A format string.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list arg;
+	char *ptr;
+	int i = 0;
+
+	va_start(arg, format);
+	while (format && *format)
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(arg, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(arg, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(arg, double));
+				break;
+			case 's':
+				ptr = va_arg(arg, char *);
+				if (ptr == NULL)
+					ptr = "(nil)";
+				printf("%s", ptr);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		if (*(format + 1))
+			printf(", ");
+		i++;
+	}
+	va_end(arg);
+	printf("\n");
+}
