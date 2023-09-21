e668dfe580aa02e8ed72cf5316da3ed56fca4502
mohamed_kamal <kmido9729@gmail.com>
2023-09-21 15:44:06 +0200

    linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,18 +6,13 @@
 */
 size_t print_list(const list_t *h)
 {
+	const list_t *moo = h;
 	size_t print = 0;
 
-	for (; h != NULL; h = h->next, print++)
+	for (moo = h; moo != NULL; moo = moo->next)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("%s\n", h->str);
-		}
+		moo->str ? printf("[%d] %s\n", moo->len, moo->str) : printf("[0] (nil)\n");
+		print++;
 	}
 	return (print);
 }
