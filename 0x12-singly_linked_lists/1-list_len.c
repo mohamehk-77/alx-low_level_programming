35209712c8573013a7bf3d8f2cc5291f3bc8c21f
mohamed_kamal <kmido9729@gmail.com>
2023-09-22 00:48:43 +0200

    linked lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- * list_len - function that returns the number of elements
- * @h: a pointer
- * Retrun: 0
+ * list_len - func
+ * @h: pointer
+ * Return: 0
 */
 size_t list_len(const list_t *h)
 {
