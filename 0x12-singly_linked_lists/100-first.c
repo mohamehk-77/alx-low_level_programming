82c44507f57e8085b54751ad2e7d76e963f3d879
mohamed_kamal <kmido9729@gmail.com>
2023-09-22 02:20:38 +0200

    linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * print_main - starting point
+ */
+void print_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
