dacc8b7fe6fe3185681b61362935586cba57f279
mohamed_kamal <kmido9729@gmail.com>
2023-09-22 02:29:25 +0200

    linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,7 @@
 /**
  * print_main - starting point
  */
-void print_main(void)
+void print_main(void) __attribute__ ((constructor))
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
