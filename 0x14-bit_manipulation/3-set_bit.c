f6557a41b0a3eaac36c665c80e2cc65122d66d94
mohamed_kamal <kmido9729@gmail.com>
2023-09-28 22:14:20 +0200

    bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,7 +10,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= (sizeof(unsigned long int) * 8))
 	{
 		return (-1);
-	*n |= (1 << index);
+		*n |= (1 << index);
+		return (1);
 	}
-	return (1);
 }
