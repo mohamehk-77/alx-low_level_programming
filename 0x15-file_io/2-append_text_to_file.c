8c42256e5fc64cdd503ae01592f16bf266094647
mohamed_kamal <kmido9729@gmail.com>
2023-10-03 00:06:43 +0200

    file i/0

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * append_text_to_file - function that convert
+ * @filename: 1
+ * @text_content: 2
+ * Return: Always 0
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len = 0;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	while (text_content && text_content[len])
+		len++;
+	write(fd, text_content, len);
+	close(fd);
+	return (1);
+}
