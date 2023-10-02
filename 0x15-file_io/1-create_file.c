d4c377a38248cf6b147a655776158b4d011317ae
mohamed_kamal <kmido9729@gmail.com>
2023-10-02 23:57:12 +0200

    file i/0

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * create_file - function that converts a binary number to an unsigned int
+ * @filename: 1
+ * @text_content: 2
+ * Return: Always 0
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len = 0;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	while (text_content && text_content[len])
+		len++;
+	write(fd, text_content, len);
+	close(fd);
+	return (1);
+}
