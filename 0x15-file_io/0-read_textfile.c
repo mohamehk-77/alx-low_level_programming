8246cb5a03e8a43d4385b266d9c7c4f764d35801
mohamed_kamal <kmido9729@gmail.com>
2023-10-02 23:54:27 +0200

    file i/0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,40 +2,37 @@
 
 /**
  * read_textfile - function that converts a binary number to an unsigned int
- * @filename: a
- * @letters: a
- * Return: Always 0.
+ * @filename: 1
+ * @letters: 2
+ * Return: Always 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	char *buffer;
-	ssize_t num_bytes, num_word;
+	ssize_t num_bytes;
+	ssize_t i;
 
 	if (!filename)
 		return (0);
-	fd = open(filename, O_RDWR);
+	fd = open(filename, O_RDONLY, 0777);
 	if (fd == -1)
 		return (0);
 	buffer = malloc(letters * sizeof(char));
 	if (!buffer)
+		return (0);
+	num_bytes = read(fd, buffer, letters);
+	if (num_bytes == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-	num_bytes = read(fd, buffer, letters);
-	if (num_bytes == -1)
+	for (i = 0; i < num_bytes; i++)
 	{
-		/*free(buffer);*/
-		return (0);
+		write(STDOUT_FILENO, &buffer[i], 1);
 	}
-	num_word = write(STDOUT_FILENO, buffer, num_bytes);
-	if (num_word == -1 || num_bytes != num_word)
-	{
+	if (close(fd) == -1)
 		return (0);
-	}
 	free(buffer);
-	close(fd);
-
-	return (num_word);
+	return (num_bytes);
 }
