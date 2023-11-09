fab35ce686389eba85bdff90b975c634e59d9ef4
Mohamed kamal <kmido9729@gmail.com>
2023-11-10 00:31:17 +0200

    back to the fucking C

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_dnodeint - func
+ * @head: 2 pointer
+ * @n: integer value
+ * Return: Pointer to the new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->prev = NULL;
+	if (*head == NULL)
+	{
+		new_node->next = NULL;
+	}
+	else
+	{
+		new_node->next = *head;
+		(*head)->prev = new_node;
+	}
+	*head = new_node;
+	return (new_node);
+}
