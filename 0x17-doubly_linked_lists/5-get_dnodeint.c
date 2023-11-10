7f6f5982a8f64e09d58cc0f512198704ff11e828
Mohamed kamal <kmido9729@gmail.com>
2023-11-10 19:40:03 +0200

    back to the fucking C

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -18,24 +18,3 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	}
 	return (NULL);
 }
-
-/* dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
-{
-	unsigned int node_diff = 0;
-	dlistint_t *main_pp = head;
-
-	while (main_pp != NULL && node_diff < index)
-	{
-		main_pp = main_pp->next;
-	}
-
-	if (node_diff == index)
-	{
-		return (main_pp);
-	}
-	else
-	{
-		return (NULL);
-	}
-}
-*/
